78a9e0a32fa8fc3971a37de1c4f3a1d44a8fb84d
钰 <2314709037@qq.com>
2023-09-27 23:00:33 +0800

    dev v0.2 完善目录结构

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("hello gin")
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello gin")
+	})
+	r.Run(":8080")
 }
